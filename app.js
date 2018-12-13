dabf0586f3da9be37e20faba79936238d28b4c6a
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-13 21:33:44 +0100

    added router files for admin and user

--- app.js
+++ app.js
@@ -5,13 +5,6 @@ const bodyParser = require("body-parser");
 // configure express to use bodyParser json method
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send({
-        name: "charles",
-        age:  29
-    });
-});
-
 // listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
