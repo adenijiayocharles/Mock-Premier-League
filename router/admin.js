6f6adca1364b90d03ce6041139ec804101461559
Adeniji Ayo Charles <adenijiayocharles@gmail.com>
2018-12-14 23:37:46 +0100

    refactored admin signup and made admin login

--- router/admin.js
+++ router/admin.js
@@ -4,13 +4,26 @@ const bcryptjs = require("bcryptjs");
 const Admin = require("../models/adminModels");
 
 
+
 router.post("/admin/signup", (req, res) => {
-    // if no data in request body
-    if(Object.keys(req.body).length === 0){
+    if(req.body.name === undefined){
         return res.status(400).json({
-            message: "Request body is missing"
-        });
-    }   
+            message: "Admin name can not be empty"
+        });        
+    }
+
+
+    if(req.body.email === undefined){
+        return res.status(400).json({
+            message: "Email address can not be empty"
+        });        
+    }  
+    
+    if(req.body.password === undefined){
+        return res.status(400).json({
+            message: "Password can not be empty"
+        });        
+    }    
 
     //check if email is valid
     if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email))){
@@ -24,6 +37,7 @@ router.post("/admin/signup", (req, res) => {
     .then(admin => {
         if(admin.length >= 1){
             return res.status(409).json({
+                status: false,
                 message: "Email already exists"
             });
         }
@@ -33,6 +47,7 @@ router.post("/admin/signup", (req, res) => {
     bcryptjs.hash(req.body.password, 10, (err, hash) => {
         if(err) {
             return res.status(500).json({
+                status: false,
                 error: err
             });
         }else{
@@ -43,20 +58,68 @@ router.post("/admin/signup", (req, res) => {
             });
             admin.save()
             .then(result => {
-                res.status(201).json({
+                res.status(200).json({
                     status: true,
-                    message: "Admin details created",
-                    result: {
-                        name: result.name,
-                        email: result.email
-                    }
+                    message: "Admin details created"
                 });
             })
             .catch(err => {
-                console.log(err.code);
+                status: false,
                 res.status(500).json(err)
             })
         }
+    })
+});
+
+router.post("/admin/login", (req, res) => {
+
+    if(req.body.email === undefined){
+        return res.status(400).json({
+            message: "Email address can not be empty"
+        });        
+    }  
+    
+    if(req.body.password === undefined){
+        return res.status(400).json({
+            message: "Password can not be empty"
+        });        
+    }
+
+    Admin.find({email: req.body.email})
+    .then(admin => {
+        if(admin.length < 1){
+            res.status(401).json({
+                status: false,
+                message: "Auth failed"
+            })
+        }
+
+        bcryptjs.compare(req.body.password, admin[0].password, (err, result) => {
+            // comparison failed
+            if(err){
+                res.status(401).json({
+                    status: false,
+                    message: "Auth failed"
+                })                
+            }
+
+            //comparison successful
+            if(result){
+                return res.status(200).json({
+                    message: "Auth successful"
+                })
+            }
+
+            //error
+            res.status(401).json({
+                message: "Auth failed"
+            })
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
     })
 });
 module.exports = router;
